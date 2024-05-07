1e30a88deca936d1f47b64c80fc8f58d4387288a
yosho <yleecruz@gmail.com>
2024-05-07 11:07:35 -0400

    index moved

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 const cors = require("cors");
-const router_v1 = require("./src/api/router");
+const router_v1 = require("./api/router");
 
 const app = express();
 
@@ -15,7 +15,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.get("/", (req, res) => {
 	res.sendFile(path.join(__dirname, "public", "index.html"));
 });
- 
+
 app.use("/api/v1", router_v1);
 
 const port = process.env.PORT || 3000;
